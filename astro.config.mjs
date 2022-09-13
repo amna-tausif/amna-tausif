205f7b213b202ed9a12a656afe824f4d595d03dd
Amna Tausif <amnatausif19@gmail.com>
2022-09-13 21:46:35 +0500

    Update site in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
 
 export default defineConfig({
-    site: "https://mhamza.dev",
+    site: "https://amna-tausif.github.io/",
     integrations: [
         react(),
         tailwind({
